1a207aeda5cbdc222b50b42150c937f82a66e496
Martin Ceska personal <32921308+MartinCeska@users.noreply.github.com>
2025-11-18 13:55:58 +0100

    Create resources.ts
    Amplify auth

---
+++ src/aplify/auth/resources.ts
@@ -0,0 +1,12 @@
+{
+  "Version": "2025-11-18",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": ["amplify.eu-south-1.amazonaws.com","amplify.amazonaws.com"]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
